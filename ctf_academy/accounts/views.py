c9c12f57e8c0e5ff82ce928dc626b26493ddb20e
MystoganF <159971401+MystoganF@users.noreply.github.com>
2025-10-15 14:02:56 +0800

    turned on @admin in dashboard in settings.py

--- ctf_academy/accounts/views.py
+++ ctf_academy/accounts/views.py
@@ -123,6 +123,6 @@ def is_admin(user):
     return user.is_superuser
 
 @login_required
-#@user_passes_test(is_admin)
+@user_passes_test(is_admin)
 def admin_dashboard_page(request):
     return render(request, "accounts/admin_dashboard.html")
